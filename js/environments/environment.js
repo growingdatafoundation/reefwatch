aa332f08624d45b94c4ad115c09ce33fbb7d4a90
Anoop Mishra <mishraanoop52@yahoo.com>
2018-06-12 21:43:12 +0930

    remove js/environments/environment.js

--- js/environments/environment.js
+++
@@ -1,5 +0,0 @@
-export const env = {
-    region: 'ap-southeast-2', // Region
-    userPoolId: 'ap-southeast-2_clBl82o9h', // User Pool Id
-    clientId: '1i6cm9qk9bqck2mi5vjvmf87v' // App Client Id
-};
