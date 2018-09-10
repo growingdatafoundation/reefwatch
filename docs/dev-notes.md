89f1a6b570ca0ab75fd5cc18122bf73a706c1c73
Joerg Boeselt <joerg@brightcookie.com.au>
2018-09-10 14:04:49 +0930

    #1 tooling, docs, elint rules

--- docs/dev-notes.md
+++ docs/dev-notes.md
@@ -1,6 +1,14 @@
 
-lint to error.log
+* lint to error.log
 
-```
+```bash
 npm run eslint > ./eslint.errors
+```
+
+* shortened log of today's commits
+
+```bash
+git log --pretty="%h %s" --since=midnight
+# 44e1f20 #1 docs
+# d07968d #2 add react-table-bootstrap css
 ```
--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ module.exports = {
         loaders: [
             { test: /\.js$/, exclude: /node_modules/,  loader: 'babel-loader?presets[]=es2015&presets[]=react' }, //include: [ path.resolve("./node_modules/form-generator-react/dist") ],
             { test: /\.(css|scss})$/, loader: 'style-loader!css-loader'},
+            { test: /\.json$/, loaders: ['json']},
             { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,  exclude: /node_modules/,loader: "file" },
             { test: /\.(woff|woff2)$/,  exclude: /node_modules/, loader:"url?prefix=font/&limit=5000" },
             { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,  exclude: /node_modules/, loader: "url?limit=10000&mimetype=application/octet-stream" },
@@ -19,7 +20,7 @@ module.exports = {
             { test: /jquery\.js$/, loader: 'expose?jQuery' },
             { test: /jquery\.js$/, loader: 'expose?$' },
             { test: /jquery\..*\.js/, loader: "imports?$=jquery,jQuery=jquery,this=>window" },
-            { test: /\.js/, exclude: /node_modules/, loader: "eslint-loader" },
+            { test: /\.js/, exclude: /(node_modules|bundle.js)/, loader: "eslint-loader" },
             { test: /\.png$/, loader: "url-loader?mimetype=image/png" }
         ]
     }
